88726e424ccc462d5af2e466997e2bc58b72ea23
cpowers <cpowers@DELOITTE.com>
2016-01-24 23:20:22 -0500

    ngForms

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
     .controller('MainCtrl', function ($scope) {
         $scope.gender = '';
 
+        $scope.formIsVisible = true;
 
         $scope.genderAppropriateLanguage = {
-            subject: "",
-            pronoun: "",
-            possessive: ""
+            subject: "he/she",
+            pronoun: "him/her",
+            possessive: "his/her"
         }
 
         $scope.changeGender = function (gender) {
@@ -16,11 +17,31 @@ angular.module('myApp', [])
                 $scope.genderAppropriateLanguage.pronoun = 'her';
                 $scope.genderAppropriateLanguage.possessive = 'her';
                 $scope.gender = "";
-            } else if (gender == 'male') {
+            } else {
                 $scope.genderAppropriateLanguage.subject = 'he';
                 $scope.genderAppropriateLanguage.pronoun = 'him';
                 $scope.genderAppropriateLanguage.possessive = 'his';
                 $scope.gender = "";
             }
         }
+
+        $scope.submit = function () {
+            $scope.formIsVisible = false;
+        };
+
+        $scope.startOver = function () {
+            $scope.formIsVisible = true;
+            $scope.maleName = "";
+            $scope.jobTitle = "";
+            $scope.tediousTask = "";
+            $scope.dirtyTask = "";
+            $scope.celebrity = "";
+            $scope.uselessSkill = "";
+            $scope.obnoxiuousCelebrity = "";
+            $scope.hugeNumber = "";
+            $scope.adjective = "";
+            $scope.madForm.$submitted = false;
+            $scope.madForm.$setPristine();
+        }
+
     });
